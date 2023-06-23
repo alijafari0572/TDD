4fbaf0f83952b82216290216f239e9e1f26082da
alijafari0572 <alijafari0572@gmail.com>
2023-06-23 20:48:30 +0330

    Initial commit

---
+++ UnitTestProject1/Calculator/SalaryCalculator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+	public class SalaryCalculator
+	{
+		public decimal GetAnnnualSalary(decimal hourlyWage)
+		{
+			const int HourInYear = 2080;
+			decimal annualSalary = hourlyWage * HourInYear;
+			return annualSalary;
+		}
+	}
+}
---
+++ UnitTestProject1/UnitTestProject1/CalculatorTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Calculator;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SalaryCalculatorTestProject
+{
+	[TestClass]
+	public class CalculatorTests
+	{
+		[TestMethod]
+		public void AnnualSalaryTest()
+		{
+			//Arrange
+			SalaryCalculator sc = new SalaryCalculator();
+
+			//Act
+			decimal annualSalary = sc.GetAnnnualSalary(50);
+
+			//Assert
+			Assert.AreEqual(104000, annualSalary);
+		}
+	}
+}
